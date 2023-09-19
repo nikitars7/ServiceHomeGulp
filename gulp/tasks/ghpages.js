788e4e56b787097f0b22da3b15609da8bf8b2e46
Nikita <nikita_rs7@ukr.net>
2023-09-19 16:40:02 +0300

    Update 2023-09-19T13:40:00.450Z

--- gulp/tasks/ghpages.js
+++
@@ -1,8 +0,0 @@
-import ghPages from "gulp-gh-pages";
-
-export const ghPagesTask = () => {
-   app.gulp.task('deploy' ,() => {
-      return app.gulp.src(`./${buildFolder}/**/*`)
-      .pipe(ghPages());
-   })
-}
--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import * as ndevFunctions from './modules/functions.js';
-import { openMenu } from './burger.js';
-import {useDynamicAdapt} from './dynamicAdapt.js'
-ndevFunctions.isWebp();
-
-openMenu();
-useDynamicAdapt();
