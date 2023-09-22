192fefdbd20c924049e2a9ab10241e94bf3e81bc
Nikita <nikita_rs7@ukr.net>
2023-09-23 00:44:14 +0300

    observerapi

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
 import * as ndevFunctions from './modules/functions.js';
 import { openMenu } from './burger.js';
 import {useDynamicAdapt} from './dynamicAdapt.js'
+import { smoothScrollTo } from './smoothAnchors.js';
+import { isTitleAnimated, sectionInterObserver } from './sectionObserver.js';
 ndevFunctions.isWebp();
 
 openMenu();
 useDynamicAdapt();
+smoothScrollTo();
+sectionInterObserver();
+isTitleAnimated();
